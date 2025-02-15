9ebef57e98e04849a15d4e37ba6cf1b23cdab7ba
rahmanhabibur26 <habib264521@gmail.com>
2025-02-15 15:39:08 +0600

    flight route design added

--- src/components/home/flight-route.jsx
+++ src/components/home/flight-route.jsx
@@ -1,8 +1,41 @@
+'use client';
 import Link from 'next/link';
-import React from 'react';
 import { Button } from '../ui/button';
+import { useState } from 'react';
+import { flightRouteData } from '@/services/flight-route';
+import planeIcon from '@/assets/icon/plane.svg';
+import Image from 'next/image';
+
+const FlightRouteCard = ({ to, from }) => {
+  return (
+    <Link
+      href="#"
+      className="grid grid-cols-3 items-center  bg-lightGray/20 rounded-lg py-4 px-3.5 group border border-transparent hover:bg-white hover:shadow hover:border hover:border-primary transition-all"
+    >
+      <div>
+        <h3 className="font-medium truncate group-hover:text-primary transition-colors delay-100">{to.destination}</h3>
+        <p className="truncate text-black/50 text-xs">{to.airport}</p>
+      </div>
+      <div className='justify-self-center'>
+        <Image src={planeIcon} alt="flight icon" className="w-6 h-6" />
+      </div>
+      <div>
+        <h3 className="font-medium truncate group-hover:text-primary transition-colors delay-100">{from.destination}</h3>
+        <p className="truncate text-black/50 text-xs">{from.airport}</p>
+      </div>
+    </Link>
+  );
+};
 
 const FlightRoute = () => {
+  const [data, setData] = useState(flightRouteData['domestic']);
+  const [activeTab, setActiveTab] = useState('domestic');
+
+  const handleTabChange = (tab) => {
+    setActiveTab(tab);
+    setData(flightRouteData[tab]);
+  };
+
   return (
     <div className="max-w-7xl mx-auto pb-24">
       <div className="text-center mb-10">
@@ -21,10 +54,38 @@ const FlightRoute = () => {
           </Link>
         </p>
 
-        <div className='pt-7'>
-          <Button size="lg" className="shadow-none mr-3">Domestic</Button>
-          <Button variant="secondary" size="lg" className="bg-lightGray shadow-none hover:bg-lightGray">International</Button>
+        <div className="pt-7">
+          <Button
+            variant={activeTab === 'domestic' ? 'default' : 'secondary'}
+            size="lg"
+            className={`shadow-none mr-3 ${
+              activeTab === 'domestic'
+                ? ''
+                : 'text-black/50 bg-lightGray hover:bg-lightGray/50'
+            } `}
+            onClick={() => handleTabChange('domestic')}
+          >
+            Domestic
+          </Button>
+          <Button
+            variant={activeTab === 'international' ? 'default' : 'secondary'}
+            size="lg"
+            className={`shadow-none ${
+              activeTab === 'domestic'
+                ? 'text-black/50 bg-lightGray hover:bg-lightGray/50'
+                : ''
+            } `}
+            onClick={() => handleTabChange('international')}
+          >
+            International
+          </Button>
         </div>
+      </div>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+        {data.map((route, index) => (
+          <FlightRouteCard key={index} {...route} />
+        ))}
       </div>
     </div>
   );
---
+++ src/services/flight-route.js
@@ -0,0 +1,156 @@
+export const flightRouteData = {
+  domestic: [
+    {
+      to: {
+        destination: 'Dhaka',
+        airport: 'Hazrat Shahjalal International Airport',
+      },
+      from: {
+        destination: 'Cox’s Bazar',
+        airport: 'Cox’s Bazar Airport',
+      },
+    },
+    {
+      to: {
+        destination: 'Dhaka',
+        airport: 'Hazrat Shahjalal International Airport',
+      },
+      from: {
+        destination: 'Jashore',
+        airport: 'Jashore Airport',
+      },
+    },
+    {
+      to: {
+        destination: 'Dhaka',
+        airport: 'Hazrat Shahjalal International Airport',
+      },
+      from: {
+        destination: 'Chattogram',
+        airport: 'Shah Amanat Airport',
+      },
+    },
+    {
+      to: {
+        destination: 'Dhaka',
+        airport: 'Hazrat Shahjalal International Airport',
+      },
+      from: {
+        destination: 'Shylhet',
+        airport: 'Osmany International Airport',
+      },
+    },
+    {
+      to: {
+        destination: 'Dhaka',
+        airport: 'Hazrat Shahjalal International Airport',
+      },
+      from: {
+        destination: 'Saidpur',
+        airport: 'Saidpur Airport',
+      },
+    },
+    {
+      from: {
+        destination: 'Dhaka',
+        airport: 'Hazrat Shahjalal International Airport',
+      },
+      to: {
+        destination: 'Cox’s Bazar',
+        airport: 'Cox’s Bazar Airport',
+      },
+    },
+    {
+      from: {
+        destination: 'Dhaka',
+        airport: 'Hazrat Shahjalal International Airport',
+      },
+      to: {
+        destination: 'Jashore',
+        airport: 'Jashore Airport',
+      },
+    },
+  ],
+  international: [
+    {
+      to: {
+        destination: 'Dhaka',
+        airport: 'Hazrat Shahjalal International Airport',
+      },
+      from: {
+        destination: 'Bangkok',
+        airport: 'Suvarnabhumi Airport',
+      },
+    },
+    {
+      to: {
+        destination: 'Dhaka',
+        airport: 'Hazrat Shahjalal International Airport',
+      },
+      from: {
+        destination: 'Singapore',
+        airport: 'Changi Airport',
+      },
+    },
+    {
+      to: {
+        destination: 'Dhaka',
+        airport: 'Hazrat Shahjalal International Airport',
+      },
+      from: {
+        destination: 'Dubai',
+        airport: 'Dubai International Airport',
+      },
+    },
+    {
+      to: {
+        destination: 'Dhaka',
+        airport: 'Hazrat Shahjalal International Airport',
+      },
+      from: {
+        destination: 'Kolkata',
+        airport: 'Netaji Subhas Chandra Bose International Airport',
+      },
+    },
+    {
+      to: {
+        destination: 'Dhaka',
+        airport: 'Hazrat Shahjalal International Airport',
+      },
+      from: {
+        destination: 'Kolkata',
+        airport: 'Netaji Subhas Chandra Bose International Airport',
+      },
+    },
+    {
+      from: {
+        destination: 'Dhaka',
+        airport: 'Hazrat Shahjalal International Airport',
+      },
+      to: {
+        destination: 'Kolkata',
+        airport: 'Netaji Subhas Chandra Bose International Airport',
+      },
+    },
+    {
+      to: {
+        destination: 'Dhaka',
+        airport: 'Hazrat Shahjalal International Airport',
+      },
+      from: {
+        destination: 'New York',
+        airport: 'John F. Kennedy International Airport',
+      },
+    },
+    {
+      to: {
+        destination: 'Dhaka',
+        airport: 'Hazrat Shahjalal International Airport',
+      },
+      from: {
+        destination: 'London',
+        airport: 'Heathrow Airport',
+      },
+    },
+  ],
+};
